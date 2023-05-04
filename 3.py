a3ffaa82da5f0bd099094e4b8d92b0565331afaa
viktoriatyapkova <114080019+viktoriatyapkova@users.noreply.github.com>
2023-05-04 11:31:36 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,3 +4,12 @@ a.insert(0, b)
 c = " ".join(a)
 print(c)
 # по-другому я не понимаю 
+
+правильное выполнение:
+  def shift(arr):
+    buff = arr[-1]
+    for i in range(len(arr)-1, 0, -1):
+      arr [i] = arr [i - 1]
+    arr [0] = buff
+    return arr
+print(shift([1, 2, 3, 4, 5]))  
